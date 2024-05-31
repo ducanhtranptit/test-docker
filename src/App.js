d1ebf3f4bbfe38278af6ec3bbcffcdfa5fb6889a
ducanhtranptit <tda.ducanh@gmail.com>
2024-05-31 11:22:34 +0700

    update code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Test Docker</h1>
-    </div>
-  );
+	const [users, setUsers] = useState([]);
+
+	useEffect(() => {
+		fetch("https://test30.tttn.vn/users")
+			.then((response) => response.json())
+			.then((data) => setUsers(data))
+			.catch((error) => console.error("Error fetching users:", error));
+	}, []);
+
+	return (
+		<div className="App">
+			<h1>Test Docker</h1>
+			<h2>User List:</h2>
+			<ul>
+				{users.map((user) => (
+					<li key={user.id}>{user.firstName}</li>
+				))}
+			</ul>
+		</div>
+	);
 }
 
 export default App;
